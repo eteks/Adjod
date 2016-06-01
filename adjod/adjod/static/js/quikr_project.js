63fd4681b47354f77447e44aedca1431b8359745
kalaiR <kalaimca.gs@gmail.com>
2016-06-01 19:32:53 +0530

    update image edit code

--- adjod/adjod/static/js/quikr_project.js
+++ adjod/adjod/static/js/quikr_project.js
@@ -1294,6 +1294,35 @@ $( document ).ready(function() {
    //  // alert($.cookie("edit_image_path"));
    // });
 
+
+     $('.edit_after_save').each(function(){       
+        var img =new Image();
+        img.src = $(this).attr('src');
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        var MAX_WIDTH = 400;
+        var MAX_HEIGHT = 300;
+        var width = img.width;
+        var height = img.height;
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
+        }
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+        dataUrl = canvas.toDataURL("image/png");
+        $(this).attr('src',dataUrl);
+     });
+
 });
 $( window ).load(function() {
   if($('.simpleFilePreview_multiUI').hasClass('edit_image_available')){
